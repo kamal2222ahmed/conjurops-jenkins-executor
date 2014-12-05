78833726884312b0ebc2358789a34962beab6124
Kevin Gilpin <kgilpin@gmail.com>
2014-12-05 01:12:06 -0500

    get rvm to work, add buncker

--- recipes/default.rb
+++ recipes/default.rb
@@ -67,15 +67,48 @@ execute "curl -sSL https://get.rvm.io | bash -s stable" do
   creates '/var/lib/jenkins/.rvm'
 end
 
+# http://stackoverflow.com/questions/16563115/how-to-install-rvm-system-requirements-without-giving-sudo-for-rvm-user
+# Dependencies will fail to install anyway because Jenkins doesn't have sudo access
+bash "rvm autolibs fail" do
+  user 'jenkins'
+end
+  
+# Ruby requirements
+%w(libreadline6-dev zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libgdbm-dev libtool pkg-config libffi-dev).each do |pkg|
+  package pkg
+end
+
 %w(ruby-1.9.3 ruby-2.0.0).each do |ruby|
   bash "rvm install #{ruby}" do
     user 'jenkins'
+    environment 'HOME' => '/var/lib/jenkins'
   end
   %w(bundler conjur-cli).each do |gem|
     bash "rvm #{ruby} do gem install #{gem}" do
       user 'jenkins'
+      environment 'HOME' => '/var/lib/jenkins'
     end
   end
 end
 
 include_recipe "nodejs"
+
+execute "git clone git@github.com:conjurinc/buncker.git" do
+  cwd '/var/lib/jenkins'
+  user 'jenkins'
+end
+
+bash "make" do
+  cwd '/var/lib/jenkins/buncker'
+end
+
+file "/etc/sudoers.d/jenkins" do
+  content <<-SUDOERS
+jenkins ALL=(ALL) NOPASSWD: /usr/bin/docker
+jenkins ALL=(ALL) NOPASSWD: /var/lib/jenkins/buncker/buncker.sh
+  SUDOERS
+end
+
+service "jenkins" do
+  action [ :enable, :restart ]
+end
