fecf7ff35956a6bb05343c7e0408d51f3a8649f4
Dustin Collins <dustinrcollins@gmail.com>
2014-12-16 11:07:36 -0500

    [#84612836] Default to m3.medium, tag AMI with git hash

--- packer.sh
+++ packer.sh
@@ -4,4 +4,6 @@ rm -rf .vendor
 
 berks vendor .vendor/cookbooks
 
-conjur env run -- packer build packer.json
+git_hash=$(git rev-parse HEAD)
+
+conjur env run -- packer build -var 'git_hash=${git_hash}' packer.json
