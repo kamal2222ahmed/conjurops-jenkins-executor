034b800e54b2f37aa14cd3f649ddab8c9eb0c86c
Dustin Collins <dustinrcollins@gmail.com>
2014-12-17 14:11:00 -0500

    [#84612836] Install ChefDK and the kitchen-docker driver

---
+++ recipes/_chefdk.rb
@@ -0,0 +1,17 @@
+# Install the chef dk and tools
+
+remote_file '/tmp/chefdk.deb' do
+  source 'https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chefdk_0.3.5-1_amd64.deb'
+  checksum 'a81c3dfad698fbb19a6c8e3dd65f04a15fd58a811d99db9c47e5a93e368f341d'
+end
+
+dpkg_package 'chefdk' do
+  source '/tmp/chefdk.deb'
+  action :install
+end
+
+execute 'install the kitchen-docker driver' do
+  command 'chef gem install kitchen-docker'
+
+  not_if 'chef gem list | grep kitchen-docker'
+end
