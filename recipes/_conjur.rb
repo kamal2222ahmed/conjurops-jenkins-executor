4b8bd2e013f1ff7b555a5d11e3ababa79534aeea
Dustin Collins <dustinrcollins@gmail.com>
2014-12-18 14:33:40 -0500

    [#84612836] Place role and bootstrap script userdata can call

--- recipes/_conjur.rb
+++ recipes/_conjur.rb
@@ -23,3 +23,22 @@ end
 execute 'fix conjur permissions to allow non-root to run the CLI' do
   command 'chmod -R 755 /var/lib/gems/1.9.1/gems/conjur-*'
 end
+
+directory '/var/chef/roles' do
+  recursive true
+end
+
+template '/var/chef/roles/host-identity.json' do
+  source "host-identity.erb"
+  variables({
+    appliance_url: node['conjur']['configuration']['appliance_url'],
+    account: node['conjur']['configuration']['account'],
+    ssl_certificate: node['conjur']['configuration']['ssl_certificate']
+  })
+end
+
+cookbook_file '/opt/conjur-bootstrap.sh' do
+  source 'conjur-bootstrap.sh'
+  mode '0755'
+end
+
--- test/integration/default/serverspec/_conjur_spec.rb
+++ test/integration/default/serverspec/_conjur_spec.rb
@@ -16,4 +16,20 @@ describe 'conjurops-jenkins-slave::_conjur' do
     expect(command('which conjur').stdout).to match /\/usr\/local\/bin\/conjur/
     expect(command('conjur --version').stdout).to match /conjur version/
   end
+
+  it 'places the Chef role to bootstrap host-factory' do
+    role = '/var/chef/roles/host-identity.json'
+
+    expect(file(role)).to be_file
+    expect(file(role).content).to match /"account": "conjurops"/
+    expect(file(role).content).to match /ec2\/{{HOST_ID}}/
+    expect(file(role).content).to match /{{HOST_TOKEN}}/
+  end
+
+  it 'places the conjur-bootstrap script' do
+    bootstrap = '/opt/conjur-bootstrap.sh'
+
+    expect(file(bootstrap)).to be_file
+    expect(file(bootstrap)).to be_executable
+  end
 end
