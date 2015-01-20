665ad767298cd069e43373542c6811ad3ea3b3ea
Nikolai Sevostjanov <nikolai.sevostjanov@gmail.com>
2015-01-20 15:07:05 -0500

    Add lxc record to sudoers

--- recipes/_lxc.rb
+++ recipes/_lxc.rb
@@ -8,7 +8,9 @@ end
 
 package "lxc"
 
-if not node["virtualization"]["system"] == 'lxc'
+# better to not try run modprobe inside LXC or Docker env
+# it will not work in any way
+if not (node["virtualization"] || {})["system"] == 'lxc'
   bash "modprobe ip6table_filter" do
     user "root"
     code "modprobe ip6table_filter"
@@ -19,5 +21,14 @@ if not node["virtualization"]["system"] == 'lxc'
     user "root"
     code "echo 'ip6table_filter' >> /etc/modules"
     not_if "grep '^ip6table_filter$' /etc/modules"
+  end
+end
+
+
+%w(lxc-attach lxc-destroy lxc-start-ephemeral lxc-autostart lxc-device lxc-stop lxc-cgroup lxc-execute lxc-top lxc-checkconfig lxc-freeze lxc-unfreeze lxc-checkpoint lxc-info lxc-unshare lxc-clone lxc-ls lxc-usernsexec lxc-config lxc-monitor lxc-wait lxc-console lxc-snapshot lxc-create lxc-start).each do |cmd|
+  sudo 'lxc' do
+    user     "%sudo"
+    runas    "root"
+    commands ["/usr/bin/#{cmd} *"]
   end
 end
