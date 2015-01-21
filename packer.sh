e231e08872f0c88097c201069c49e238c8667b4d
Dustin Collins <dustinrcollins@gmail.com>
2015-01-21 13:37:37 -0500

    Also append the latest git tag to AMI metadata

--- packer.sh
+++ packer.sh
@@ -5,5 +5,6 @@ rm -rf .vendor
 berks vendor .vendor/cookbooks
 
 export GIT_HASH=$(git rev-parse HEAD)
+export GIT_TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
 
 conjur env run -- packer build $@ packer.json
