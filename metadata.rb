dd81835d21ddc4bf27e287e3b0b0b7e040e13a6f
Dustin Collins <dustinrcollins@gmail.com>
2015-01-21 13:34:24 -0500

    [#86473306] v0.1.6 w/ ChefDK installed

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'support@conjur.net'
 license          'Restricted'
 description      'Installs/Configures jenkins slave'
 long_description 'Installs/Configures jenkins slave'
-version          '0.1.5'
+version          '0.1.6'
 
 supports 'ubuntu'
 
