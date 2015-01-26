60a9299d3bf1673a65560dc4fec87c39986d2ffe
Nikolai Sevostjanov <nikolai.sevostjanov@gmail.com>
2015-01-26 21:19:47 +0300

    Add new record to suders and install lxc from PPA only if Ubuntu older than 14.10

--- recipes/_lxc.rb
+++ recipes/_lxc.rb
@@ -1,9 +1,11 @@
-apt_repository "ubuntu-lxc" do
-  uri "http://ppa.launchpad.net/ubuntu-lxc/stable/ubuntu"
-  distribution node['lsb']['codename']
-  components ["main"]
-  keyserver "keyserver.ubuntu.com"
-  key "7635B973"
+if node["platform"] == "ubuntu" and node["platform_version"].to_f < 14.10
+  apt_repository "ubuntu-lxc" do
+    uri "http://ppa.launchpad.net/ubuntu-lxc/stable/ubuntu"
+    distribution node['lsb']['codename']
+    components ["main"]
+    keyserver "keyserver.ubuntu.com"
+    key "7635B973"
+  end
 end
 
 package "lxc"
@@ -24,6 +26,9 @@ if not (node["virtualization"] || {})["system"] == 'lxc'
   end
 end
 
+# This gives no any secure, wildcard in sudoers doesn't work securely
+# So, what about password less sudo for jenkins user?
+
 %w(lxc-attach lxc-destroy lxc-start-ephemeral lxc-autostart lxc-device lxc-stop lxc-cgroup lxc-execute lxc-top lxc-checkconfig lxc-freeze lxc-unfreeze lxc-checkpoint lxc-info lxc-unshare lxc-clone lxc-ls lxc-usernsexec lxc-config lxc-monitor lxc-wait lxc-console lxc-snapshot lxc-create lxc-start).each do |cmd|
   sudo cmd do
     user     "%sudo"
@@ -31,4 +36,46 @@ end
     nopasswd true
     commands ["/usr/bin/#{cmd} *"]
   end
+end
+
+sudo 'lxc--mkdir1' do
+  user     "%sudo"
+  runas    "root"
+  nopasswd true
+  commands ["/bin/mkdir /var/lib/lxc/*/rootfs/*"]
+end
+
+sudo 'lxc--mkdir2' do
+  user     "%sudo"
+  runas    "root"
+  nopasswd true
+  commands ["/bin/mkdir -p /var/lib/lxc/*/rootfs/*"]
+end
+
+sudo 'lxc--chmod1' do
+  user     "%sudo"
+  runas    "root"
+  nopasswd true
+  commands ["/bin/chmod -R 0440 /var/lib/lxc/*/rootfs/*"]
+end
+
+sudo 'lxc--chmod2' do
+  user     "%sudo"
+  runas    "root"
+  nopasswd true
+  commands ["/bin/chmod 0755 /var/lib/lxc/*/rootfs/*"]
+end
+
+sudo 'lxc--tee1' do
+  user     "%sudo"
+  runas    "root"
+  nopasswd true
+  commands ["/usr/bin/tee /var/lib/lxc/*/rootfs/*"]
+end
+
+sudo 'lxc--tee2' do
+  user     "%sudo"
+  runas    "root"
+  nopasswd true
+  commands ["/usr/bin/tee --append /var/lib/lxc/*/fstab"]
 end
