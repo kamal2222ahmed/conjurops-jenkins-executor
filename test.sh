b7e0c1409a2ade87c69801d33a2027eafa0c9eec
Alan Potter <alanjpotter@gmail.com>
2015-07-14 10:58:51 -0400

    Use a fixed location for the private key when testing

--- test.sh
+++ test.sh
@@ -3,9 +3,16 @@
 rm -rf .kitchen
 
 export KITCHEN_LOCAL_YAML=.kitchen.ci.yml
+secrets=${SECRETS_YAML:-secrets.ci.yml}
 
-summon -f secrets.ci.yml kitchen converge
-summon -f secrets.ci.yml kitchen verify
+# kitchen expects the SSH key to be in the same place from one run to
+# the next. Put it in a file, delete it when we're done.
+summon -f "$secrets" bash -c 'echo "$SSH_PRIVATE_KEY" > private_key'
+chmod 0600 private_key
+trap "rm private_key" EXIT
+
+summon -f "$secrets" kitchen converge
+summon -f "$secrets" kitchen verify
 sleep 10
-summon -f secrets.ci.yml kitchen destroy || true
+summon -f "$secrets" kitchen destroy || true
 
