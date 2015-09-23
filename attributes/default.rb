b8ab8b9ae42a22cc8a43ad171d15ddec892f7e4f
Dustin Collins <dustinrcollins@gmail.com>
2015-09-23 16:46:59 -0400

    packer v0.8.6

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,8 +7,8 @@ default['conjur']['configuration']['appliance_url'] = 'https://conjur-master.itp
 default['conjur']['configuration']['plugins'] = []
 default['conjur']['configuration']['ssl_certificate'] = "-----BEGIN CERTIFICATE-----\nMIIDUTCCAjmgAwIBAgIJAO4Lf1Rf2cciMA0GCSqGSIb3DQEBBQUAMDMxMTAvBgNV\nBAMTKGVjMi01NC05MS0yNDYtODQuY29tcHV0ZS0xLmFtYXpvbmF3cy5jb20wHhcN\nMTQxMDA4MjEwNTA5WhcNMjQxMDA1MjEwNTA5WjAzMTEwLwYDVQQDEyhlYzItNTQt\nOTEtMjQ2LTg0LmNvbXB1dGUtMS5hbWF6b25hd3MuY29tMIIBIjANBgkqhkiG9w0B\nAQEFAAOCAQ8AMIIBCgKCAQEAx+OFANXNEYNsMR3Uvg4/72VG3LZO8yxrYaYzc3FZ\nNN3NpIOCZvRTC5S+OawsdEljHwfhdVoXdWNKgVJakSxsAnnaj11fA6XpfN60o6Fk\ni4q/BqwqgeNJjKAlElFsNz2scWFWRe49NHlj9qaq/yWZ8Cn0IeHy8j8F+jMek4zt\ndCSxVEayVG/k8RFmYCcluQc/1LuCjPiFwJU43AGkO+yvmOuYGivsNKY+54yuEZqF\nVDsjAjMsYXxgLx9y1F7Rq3CfeqY6IajR7pmmRup8/D9NyyyQuIML83mjTSvo0UYu\nrkdXPObd/m6gumscvXMl6SoJ5IPItvTA42MZqTaNzimF0QIDAQABo2gwZjBkBgNV\nHREEXTBbgglsb2NhbGhvc3SCBmNvbmp1coIcY29uanVyLW1hc3Rlci5pdHAuY29u\nanVyLm5ldIIoZWMyLTU0LTkxLTI0Ni04NC5jb21wdXRlLTEuYW1hem9uYXdzLmNv\nbTANBgkqhkiG9w0BAQUFAAOCAQEANk7P3ZEZHLgiTrLG13VAkm33FAvFzRG6akx1\njgNeRDgSaxRtrfJq3mnhsmD6hdvv+e6prPCFOjeEDheyCZyQDESdVEJBwytHVjnH\ndbvgMRaPm6OO8CyRyNjg3YcC36T//oQKOdAXXEcrtd0QbelBDYlKA7smJtznfhAb\nXypVdeS/6I4qvJi3Ckp5sQ1GszYhVXAvEeWeY59WwsTWYHLkzss9QShnigPyo3LY\nZA5JVXofYi9DJ6VexP7sJNhCMrY2WnMpPcAOB9T7a6lcoXj6mWxvFys0xDIEOnc6\nNGb+d47blphUKRZMAUZgYgFfMfmlyu1IXj03J8AuKtIMEwkXAA==\n-----END CERTIFICATE-----\n"
 
-default['packer']['version'] = 'packer_0.7.5'
-default['packer']['checksum'] = '8fab291c8cc988bd0004195677924ab6846aee5800b6c8696d71d33456701ef6'
+default['packer']['version'] = 'packer_0.8.6'
+default['packer']['checksum'] = '2f1ca794e51de831ace30792ab0886aca516bf6b407f6027e816ba7ca79703b5'
 
 default['nginx']['sites-enabled'] = '/etc/nginx/sites-enabled/'
 default['nginx']['sites-available'] = '/etc/nginx/sites-available/'
