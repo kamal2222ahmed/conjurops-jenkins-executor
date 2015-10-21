f0ce36d88e62a0cb074a5f5f8be348a1f8157540
Alan Potter <alanjpotter@gmail.com>
2015-10-21 16:01:56 -0400

    remove virtual packages that cause the kernel to get upgraded

--- base-ami-build/image-extra.sh
+++ base-ami-build/image-extra.sh
@@ -2,6 +2,7 @@
 date
 
 version=$(uname -r)
+sudo dpkg -r linux-virtual linux-headers-generic linux-headers-virtual linux-image-virtual
 sudo apt-get install -y linux-image-extra-$version
-sudo apt-mark hold linux-image-$version linux-image-extra-$version
+
 
