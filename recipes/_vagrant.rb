a6bf3d6868da8ef39b9cb4704b3b998945d39acf
Alan Potter <alanjpotter@gmail.com>
2016-09-19 09:15:52 -0400

    pin vagrant-berkshelf to ~>4

--- recipes/_vagrant.rb
+++ recipes/_vagrant.rb
@@ -3,6 +3,11 @@ include_recipe 'vagrant'
 ['vagrant-aws', 'vagrant-ami', 'vagrant-berkshelf'].each do |p|
   vagrant_plugin p do
     user 'jenkins'
+
+    # Both sets of quotes are required to get the version passed
+    # properly to 'vagrant plugin install'
+    version "'~>4'" if p == 'vagrant-berkshelf'
+
   end
 end
 
